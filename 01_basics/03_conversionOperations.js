e8f50d2e03fb0e60b279fdf7af12fb29cbd2b141
binodgolay <146081764+binodgolay@users.noreply.github.com>
2023-09-27 06:32:43 +0000

    string concatination added

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -8,4 +8,27 @@ let fName = "33"
 
 let stringInNumber = Number(fName)
 
-console.log(typeof stringInNumber);
+// console.log(typeof stringInNumber);
+
+let isLoggedIn = null
+
+let loggedInAsBoolean = Boolean(isLoggedIn)
+
+// console.log(loggedInAsBoolean);
+// console.log(Number(isLoggedIn));
+
+/**************** OPERATIONS ***************/
+
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+let firstName = "Binod"
+
+let lastName = "Lama"
+
+console.log(firstName + " " + lastName);
+
+
+
