d156bbd237c2885292f64bcc892d47fe754b9861
binodgolay <146081764+binodgolay@users.noreply.github.com>
2023-09-27 06:04:13 +0000

    conversion Operations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,11 @@
+let newNUmber = 33
+
+let numberInString = String(newNUmber)
+
+//console.log(typeof numberInString);
+
+let fName = "33"
+
+let stringInNumber = Number(fName)
+
+console.log(typeof stringInNumber);
