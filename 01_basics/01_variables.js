d056ba9b06a972f723a914bc4cc4943174839499
binodgolay <146081764+binodgolay@users.noreply.github.com>
2023-09-27 05:19:22 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,7 @@
+const accountID = 12345;
+let accountEmail = "binod@googl.com";
+let accountState;
+
+accountEmail = "bin@binod.com";
+
+console.table([accountEmail, accountState, accountID]);
---
+++ 01_basics/02_variableTypes.js
@@ -0,0 +1,12 @@
+let accountName = "Binod";
+
+//string
+// number
+// bigInt
+// NULL
+//undefined
+
+//object
+//boolean => True / False
+
+console.log(typeof accountName);
